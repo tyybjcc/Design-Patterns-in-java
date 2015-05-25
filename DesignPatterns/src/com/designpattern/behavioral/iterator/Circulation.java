5d6d4ba4c4fe1d6072d6cab8517483ed8fdf5403
Frank Tian <tyybjcc@126.com>
2015-05-25 20:12:09 +0800

    1. add docs. Written in Chinese 2. Alter iterator pattern.

--- DesignPatterns/src/com/designpattern/behavioral/iterator/Circulation.java
+++ DesignPatterns/src/com/designpattern/behavioral/iterator/Circulation.java
@@ -1,15 +1,18 @@
 package com.designpattern.behavioral.iterator;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
-public class Circulation<T extends Object> implements Iterable{
+//Iterable<T> generic type T mustn't be ignored
+public class Circulation<T> implements Iterable<T>{
 	private List<T> list = new ArrayList<T>();
 
 	@Override
-	public Iterator iterator() {
-		return new CirculationIterator(this.list);
+	public Iterator<T> iterator() {
+		return new CirculationIterator();
 	}
 	
 	public T get(int index) {
@@ -31,7 +34,55 @@ public class Circulation<T extends Object> implements Iterable{
 	public void set(int index, T e) {
 		this.list.set(index, e);
 	}
+	
+	
+	//iterator should be an inner class
+	private class CirculationIterator implements Iterator<T>{
+		private int pos;
+		private int size;
+		Set<Integer> deleted = new HashSet<Integer>();
+		private boolean nextNotFoundTag = false;
+		
+		public CirculationIterator(){
+			pos = 0;
+			size = list.size();
+		}
 
+		@Override
+		public boolean hasNext() {
+			return (size!=0);
+		}
+
+		@Override
+		public T next() {
+			if(this.hasNext()) {
+				T rtn = list.get(this.pos);
+				findNext();
+				return rtn;
+			}else{
+				return null;
+			}
+		}
+		
+		//fake remove: mark removed elements instead of real removing from Circulation
+		@Override
+		public void remove() {
+			size--;
+			deleted.add(new Integer(pos));
+			this.findNext();
+		}
+		
+		private void findNext() {
+			for(int i=0; i<list.size(); i++) {
+				pos = (++pos)%list.size();
+				if(!deleted.contains(pos)){
+					return;
+				}
+			}
+			nextNotFoundTag = true;
+		}
+
+	}
 	
 
 }
--- DesignPatterns/src/com/designpattern/behavioral/iterator/Client.java
+++ DesignPatterns/src/com/designpattern/behavioral/iterator/Client.java
@@ -11,20 +11,27 @@ public class Client {
 		}
 		
 		//use iterator
-		Iterator iterator = cir.iterator(); 
+		Iterator<Integer> iterator = cir.iterator(); 
 		int maxCount=1;
 		while(iterator.hasNext()) {
 			System.out.println(iterator.next());
 			if(++maxCount>15) break;
 		}
 		
+		//Iterable<T> interface implementation is a must, or "for:" cannot be used here.
+		for(Integer i:cir) {
+			
+		}
+		
+		/*
+		//fake remove
 		System.out.println("\nremove:");
 		maxCount=1;
 		while(iterator.hasNext()) {
 			System.out.println(maxCount++);
 			iterator.remove();
 		}
-		
+		*/
 	}
 
 }
